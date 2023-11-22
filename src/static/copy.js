f5485ee2c88cbca395d89eb81bd55663f4ae4427
werdffelynir <werdffelynir@gmail.com>
2023-11-23 01:57:13 +0200

    fix #65

--- src/static/copy.js
+++ src/static/copy.js
@@ -1,14 +1,35 @@
 import typeOf from "./typeOf";
 import isNode from "./isNode";
 
-
-const copy = function (src, instance)
+/**
+ *
+ * @param src
+ * @param instance
+ * @returns {{}|ActiveX.IXMLDOMNode|Node|*[]|Date|*}
+ */
+const copy = function (src, instance = {})
 {
+    // Handle the 3 simple types, and null or undefined
+    if (null == src || "object" != typeof src) return src;
+
+    if (src instanceof Date) {
+        let tmp;
+        tmp = new Date();
+        tmp.setTime(src.getTime());
+        return tmp;
+    }
+
     if (isNode(src))
         return src.cloneNode(true);
 
-    if (Array.isArray(src))
-        return src.slice();
+    if (Array.isArray(src)){
+        // return src.slice();
+        let tmp = [];
+        for (let i = 0, len = src.length; i < len; i++) {
+            tmp[i] = copy(src[i]);
+        }
+        return tmp;
+    }
 
     if (typeof src === 'function')
         return src.bind(instance || {});
@@ -22,7 +43,7 @@ const copy = function (src, instance)
         return result;
     }
 
-    return src;
+    throw new Error("Unable to copy obj! Its type isn't supported.");
 };
 
 export default copy;
